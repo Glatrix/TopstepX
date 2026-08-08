4b72c51707168d8717a4176f2ea7ba706ecf1533
Glatrix <73367967+Glatrix@users.noreply.github.com>
2026-08-07 22:12:11 -0500

    Remove Secrets
    Prob should not have these in your code homie

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -16,7 +16,7 @@ namespace Samples
 
     static async void Run()
     {
-      var client = new TopstepBroker("arteinvolo@gmail.com", "Ou/vnGd3kmF5KDGCeSMCFVsiHZzucF5dkNLy/oLgwsM=");
+      var client = new TopstepBroker("email@example.com", "TOKEN_HERE");
 
       var loginResponse = await client.AuthLoginKey();
       Console.WriteLine($"Token: {loginResponse.token}");
